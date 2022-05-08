b8dcd9267beb88cb8657ff6f8fda0ea19fbf496a
Toka <mahdisayeri@gmail.com>
2022-05-08 14:54:00 +0430

    whrite sample for builder pattern

--- app/src/main/java/com/tokastudio/androiddesignpatterns/MainActivity.kt
+++ app/src/main/java/com/tokastudio/androiddesignpatterns/MainActivity.kt
@@ -2,10 +2,19 @@ package com.tokastudio.androiddesignpatterns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.tokastudio.androiddesignpatterns.builder.Hamburger
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val hamburger= Hamburger.Builder()
+            .ham(true)
+            .cheese(false)
+            .tomatoes(true)
+            .build()
+        hamburger.printData()
     }
 }
---
+++ app/src/main/java/com/tokastudio/androiddesignpatterns/builder/Hamburger.kt
@@ -0,0 +1,32 @@
+package com.tokastudio.androiddesignpatterns.builder
+
+class Hamburger private constructor(
+    private val bread: Boolean,
+    private val ham: Boolean,
+    private val cheese: Boolean,
+    private val tomatoes: Boolean
+) {
+
+    class Builder{
+        private val bread: Boolean= true
+        private var ham: Boolean= false
+        private var cheese: Boolean= false
+        private var tomatoes: Boolean= false
+
+//        fun bread(value: Boolean) {
+//            bread= value
+//        }
+        fun ham(value: Boolean)= apply{ ham= value }
+        fun cheese(value: Boolean)= apply{ cheese= value }
+        fun tomatoes(value: Boolean)= apply{ tomatoes= value }
+
+        fun build()= Hamburger(bread,ham,cheese,tomatoes)
+    }
+
+    fun printData(){
+        println("bread= $bread")
+        println("ham= $ham")
+        println("cheese= $cheese")
+        println("tomatoes $tomatoes")
+    }
+}
